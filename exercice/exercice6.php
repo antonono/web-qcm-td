3aab7d659d7cbcb5c9f8e815b0cb6b18a13ce538
Antonin Boinega <antoninboinega@gmail.com>
2019-02-18 11:30:31 +0100

    EXERCICE DS

--- exercice/exercice6.php
+++ exercice/exercice6.php
@@ -3,9 +3,8 @@
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-
 $M = array_shift($args);
 for ($i = 0; $i < $M; $i++) {
     $N = $args[$i];
-    echo "{$N}\n";
+    echo ($N%2 == 0 ? ($N+1) . "\r\n" : $N."\r\n");
 }

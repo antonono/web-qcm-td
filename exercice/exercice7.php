df800565fc87015c484d70c860e8a6cabde730d4
Antonin Boinega <antoninboinega@gmail.com>
2019-02-18 12:07:34 +0100

    Ajout exercice 7 DS

--- exercice/exercice7.php
+++ exercice/exercice7.php
@@ -3,13 +3,34 @@
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-
-$N = array_shift($args);
+$res = 0;
+$rep = null;
+$N = (int)array_shift($args);
 $K = array_shift($args);
 for ($i = 0; $i < $N; $i++) {
     $A = $args[$i];
+
+    if ($A[0] == "X") {
+        $rep = "A";
+    }
+    if ($A[1] == "X") {
+        $rep = "B";
+    }
+    if ($A[2] == "X") {
+        $rep = "C";
+    }
+    if ($A[3] == "X") {
+        $rep = "D";
+    }
+    if ($A[4] == "X") {
+        $rep = "E";
+    }
+
+    if ($rep == $K[$i]) {
+        $res = $res + 100/$N;
+    }
 }
 
 // Write an action using echo().
 
-echo("answer");
+echo($res);
